cd0cfa758542daa4aada3280eab4d5296cd5ee40
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-04 15:32:15 -0600

    add json

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "Build for web with custom base href"
 flutter build web --release --base-href /
+
+echo "Create robots.txt"
+echo "User-Agent: *
+Allow: /$
+Disallow: /" > build/web/robots.txt
+
+echo "Build data.json"
+cd data/buildDataJson
+npm run export:json
